e1609a0e529d0afd1753081fb86c803256e8d8f2
Cameron Brooks <ca.br@Me.com>
2025-10-25 01:50:58 -0700

    Upgrade build script to PHP 8.2 for v1 build system

--- build.sh
+++ build.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 set -e
 
+echo "Adding PHP 8.2 repository..."
+sudo add-apt-repository ppa:ondrej/php -y
+sudo apt-get update
+
+echo "Installing PHP 8.2..."
+sudo apt-get install -y php8.2 php8.2-cli php8.2-mbstring php8.2-xml php8.2-curl php8.2-zip
+
+echo "Verifying PHP version..."
+php8.2 -v
+
 echo "Installing Composer..."
-curl -sS https://getcomposer.org/installer | php
+curl -sS https://getcomposer.org/installer | php8.2
 
 echo "Installing dependencies..."
-php composer.phar install --no-dev --optimize-autoloader
+php8.2 composer.phar install --no-dev --optimize-autoloader
 
 echo "Generating static site..."
-php build-static.php
+php8.2 build-static.php
 
 echo "Build complete!"
